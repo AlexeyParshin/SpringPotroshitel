824a41aa3fbe8362cac457578f7a429e62de802d
AlexeyParshin <dip1771@gmail.com>
2017-12-04 00:09:19 +0300

    added randomizer

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,6 +1,19 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
+
+  @PostConstruct
+  void init(){
+    System.out.println("Phase 2");
+    System.out.println(repeat);
+  }
+
+  TerminatorQuoter(){
+    System.out.println("Phase 1");
+  }
+
   @InjectRandomInt(min = 2, max = 7)
   private int repeat;
   private String message;
